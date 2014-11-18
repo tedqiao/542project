40d50d690ab6082ffbaa207ec98d06913dff5453
jianqiao <tedqiao@gmail.com>
2014-11-18 09:16:36 -0500

    updata ui

--- src/ui/User_Frame.java
+++ src/ui/User_Frame.java
@@ -7,6 +7,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
 
 
 public class User_Frame extends JFrame {
@@ -22,13 +24,18 @@ public class User_Frame extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		UserInformation user = new UserInformation("zhang san","male","123123123",123213123.0);
-		user.setBounds(0, 0, 194, 278);
+		user.setBorder(new LineBorder(Color.YELLOW, 1, true));
+		user.setBounds(0, 0, 193, 278);
 		getContentPane().add(user);
 		stockPanel st = new stockPanel();
 		st.setBounds(192, 105, 400, 173);
 		JScrollPane scroll = new JScrollPane(st);
 		scroll.setBounds(192, 105, 400, 173);
 		getContentPane().add(scroll);
+		total t = new total();
+		t.setSize(400, 105);
+		t.setLocation(192, 0);
+		getContentPane().add(t);
 	}
 
 }
---
+++ src/ui/total.java
@@ -0,0 +1,32 @@
+package ui;
+
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+
+public class total extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public total() {
+		setBackground(Color.BLACK);
+		setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblTotal = new JLabel("total");
+		lblTotal.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblTotal.setForeground(Color.YELLOW);
+		lblTotal.setFont(new Font("Lucida Grande", Font.PLAIN, 46));
+		add(lblTotal, BorderLayout.NORTH);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setForeground(Color.YELLOW);
+		add(lblNewLabel, BorderLayout.WEST);
+
+	}
+
+}
