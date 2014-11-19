7ee254a5c52c39ec8126edcf576e6472b59f6dfb
Chengjiao Yang <cyang@wpi.edu>
2014-11-18 22:43:44 -0500

    create table sql and test data

---
+++ SQL/testdata/hold_testdata.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: SELECT * FROM cs542.hold
+LIMIT 0, 1000
+
+-- Date: 2014-11-18 17:16
+*/
+INSERT INTO `hold` (`userID`,`Sid`,`shares`) VALUES ('1','000001',100);
+INSERT INTO `hold` (`userID`,`Sid`,`shares`) VALUES ('1','000002',200);
+INSERT INTO `hold` (`userID`,`Sid`,`shares`) VALUES ('1','000003',111);
+INSERT INTO `hold` (`userID`,`Sid`,`shares`) VALUES ('2','000001',284);
+INSERT INTO `hold` (`userID`,`Sid`,`shares`) VALUES ('2','000002',42);
+INSERT INTO `hold` (`userID`,`Sid`,`shares`) VALUES ('3','000002',2342);
+INSERT INTO `hold` (`userID`,`Sid`,`shares`) VALUES ('3','000003',235);
+INSERT INTO `hold` (`userID`,`Sid`,`shares`) VALUES ('4','000001',235);
+INSERT INTO `hold` (`userID`,`Sid`,`shares`) VALUES ('4','000003',551);
---
+++ SQL/testdata/stock_testdata.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM cs542.stock
+LIMIT 0, 1000
+
+-- Date: 2014-11-18 17:15
+*/
+INSERT INTO `stock` (`Sid`,`Price_share`,`OverallShares`,`Variation_Range`,`Cid`) VALUES ('000001',15,2000000,0.4,'1');
+INSERT INTO `stock` (`Sid`,`Price_share`,`OverallShares`,`Variation_Range`,`Cid`) VALUES ('000002',20,6666666,0.7,'2');
+INSERT INTO `stock` (`Sid`,`Price_share`,`OverallShares`,`Variation_Range`,`Cid`) VALUES ('000003',33,454353,0.9,'3');
+INSERT INTO `stock` (`Sid`,`Price_share`,`OverallShares`,`Variation_Range`,`Cid`) VALUES ('000004',6,34634234,0.2,'4');
+INSERT INTO `stock` (`Sid`,`Price_share`,`OverallShares`,`Variation_Range`,`Cid`) VALUES ('000005',35,6345,1.1,'5');
+INSERT INTO `stock` (`Sid`,`Price_share`,`OverallShares`,`Variation_Range`,`Cid`) VALUES ('000006',44,646345,0.4,'6');
