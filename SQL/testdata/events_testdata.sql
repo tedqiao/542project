ddae135378453703838ef4a00a0a3c9e165155db
Chengjiao Yang <cyang@wpi.edu>
2014-12-02 14:25:13 -0500

    update testdata sql

--- SQL/testdata/events_testdata.sql
+++ SQL/testdata/events_testdata.sql
@@ -2,10 +2,20 @@
 -- Query: SELECT * FROM cs542.events
 LIMIT 0, 1000
 
--- Date: 2014-11-25 13:49
+-- Date: 2014-12-02 14:23
 */
-INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (1,'A celebrity hung up, the stock market crash',-0.2);
-INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (2,'Terrorist attacks in the United States , the World Trade Center has been destroyed',-0.4);
-INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (3,'The success of the six-party talks , a peaceful world.',0.3);
-INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (4,'Bubble economy , the property market crash',-0.3);
-INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (5,'Chang E 100 spacecraft was successfully launched , the positive reaction',0.1);
+INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (1,'A celebrity hung up, the stock market effected.',-0.03);
+INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (2,'Terrorist attacks in the United States , the World Trade was destroyed.',-0.1);
+INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (3,'The success of the six-party talks , a peaceful world.',0.04);
+INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (4,'Bubble economy , the property market crash.',-0.09);
+INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (5,'Chang E 100 spacecraft was successfully launched , the positive reaction.',0.05);
+INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (6,'Obama expands OPT visa program for foreign students, good news.',0.04);
+INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (7,'Food safety issue occur, people are angry.',-0.07);
+INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (8,'Chinese soccer team win the world cup, cheers!',0.06);
+INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (9,'Olympic Games month, go go go.',0.03);
+INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (10,'Depreciation of US Dollar, Wall Streeters panic.',-0.03);
+INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (11,'Large volcanic eruption in Yellowstone National park, western of United States are affected',-0.05);
+INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (12,'The new technology of pig keeping is invented, now people can eat cheap pork',0.03);
+INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (13,'New diplomatic relations between the Mars and the earth, go into the era of the Milky Way',0.07);
+INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (14,'New York blackout last for two weeks, Economic affected.',-0.04);
+INSERT INTO `events` (`eventID`,`incident`,`Variation_Range`) VALUES (15,'Global economic recovery, employment rate increased.',0.05);
